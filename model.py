ae653f64fcbc98372be0854720cde93a4c5e45e3
libraries <libraries@macbook-air-5.mynetworksettings.com>
2025-04-17 15:30:58 -0400

    Updated model.py to accomodate feature 3

--- model.py
+++ model.py
@@ -19,17 +19,17 @@ class State(str, Enum):
 
 class Event:
     
-    def __init__(self, jobj:any):
-        self.event_type:str = None
-        self.author:str = None
-        self.event_date:datetime = None
-        self.label:str = None
-        self.comment:str = None
+    def __init__(self, jobj: any):
+        self.event_type: str = None
+        self.author: str = None
+        self.event_date: datetime = None
+        self.label: str = None
+        self.comment: str = None
         
         if jobj is not None:
             self.from_json(jobj)
     
-    def from_json(self, jobj:any):
+    def from_json(self, jobj: any):
         self.event_type = jobj.get('event_type')
         self.author = jobj.get('author')
         try:
@@ -38,51 +38,57 @@ class Event:
             pass
         self.label = jobj.get('label')
         self.comment = jobj.get('comment')
-        
-        
+
+
 class Issue:
     
-    def __init__(self, jobj:any=None):
-        self.url:str = None
-        self.creator:str = None
-        self.labels:List[str] = []
-        self.state:State = None
-        self.assignees:List[str] = []
-        self.title:str = None
-        self.text:str = None
-        self.number:int = -1
-        self.created_date:datetime = None
-        self.updated_date:datetime = None
-        self.closed_date:datetime = None
-        self.timeline_url:str = None
-        self.events:List[Event] = []
-        
+    def __init__(self, jobj: any = None):
+        self.url: str = None
+        self.creator: str = None
+        self.labels: List[str] = []                  
+        self.state: State = None
+        self.assignees: List[str] = []
+        self.title: str = None
+        self.text: str = None
+        self.number: int = -1
+        self.created_date: datetime = None
+        self.closed_date: datetime = None           
+        self.updated_date: datetime = None
+        self.timeline_url: str = None
+        self.events: List[Event] = []
+
         if jobj is not None:
             self.from_json(jobj)
     
-    def from_json(self, jobj:any):
+    def from_json(self, jobj: any):
         self.url = jobj.get('url')
         self.creator = jobj.get('creator')
-        self.labels = jobj.get('labels',[])
+        self.labels = [lbl['name'] for lbl in jobj.get('labels', []) if isinstance(lbl, dict) and 'name' in lbl]
+
         self.state = State[jobj.get('state')]
-        self.assignees = jobj.get('assignees',[])
+        self.assignees = jobj.get('assignees', [])
         self.title = jobj.get('title')
         self.text = jobj.get('text')
+
         try:
-            self.number = int(jobj.get('number','-1'))
+            self.number = int(jobj.get('number', '-1'))
         except:
             pass
+
         try:
             self.created_date = parser.parse(jobj.get('created_at'))
         except:
             pass
+
+        try:
+            self.closed_date = parser.parse(jobj.get('closed_at'))  
+        except:
+            self.closed_date = None
+
         try:
             self.updated_date = parser.parse(jobj.get('updated_at'))
         except:
             pass
-        try:
-            self.closed_date = parser.parse(jobj.get('closed_at'))
-        except:
-            pass
+
         self.timeline_url = jobj.get('timeline_url')
-        self.events = [Event(jevent) for jevent in jobj.get('events',[])]
+        self.events = [Event(jevent) for jevent in jobj.get('events', [])]
